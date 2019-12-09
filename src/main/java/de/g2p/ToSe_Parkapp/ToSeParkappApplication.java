ce2a82a3db925d62a6dc013b1f13823b17fa110c
Maximilian Moeginger <moegin03@ads.uni-passau.de>
2019-12-09 19:17:53 +0100

    Initial commit

---
+++ src/main/java/de/g2p/ToSe_Parkapp/ToSeParkappApplication.java
@@ -0,0 +1,13 @@
+package de.g2p.ToSe_Parkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToSeParkappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToSeParkappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/g2p/ToSe_Parkapp/ToSeParkappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.g2p.ToSe_Parkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToSeParkappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
