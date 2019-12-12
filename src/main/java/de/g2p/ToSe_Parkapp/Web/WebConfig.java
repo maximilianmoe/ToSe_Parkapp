569a5497fa59447aae98688592fa5563cff1d2df
Matthias <accounts@matthias-scherzer.de>
2019-12-12 18:54:29 +0100

    WebConfig

--- src/main/java/de/g2p/ToSe_Parkapp/Web/WebConfig.java
+++ src/main/java/de/g2p/ToSe_Parkapp/Web/WebConfig.java
@@ -1,7 +1,14 @@
 package de.g2p.ToSe_Parkapp.Web;
 
-//testcomment
-public class WebConfig {
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html");
 
 
+    }
 }
