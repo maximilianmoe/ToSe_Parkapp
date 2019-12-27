a9d3751830f0ac5d90678103c5eee74537d8de88
Maximilian Moeginger <moegin03@ads.uni-passau.de>
2019-12-27 13:36:25 +0100

    Registrieren in NutzerController erweitert

--- src/main/java/de/g2p/ToSe_Parkapp/Controller/WebController.java
+++ src/main/java/de/g2p/ToSe_Parkapp/Controller/WebController.java
@@ -4,13 +4,9 @@ import de.g2p.ToSe_Parkapp.Entities.Nutzer;
 import de.g2p.ToSe_Parkapp.Entities.Standort;
 import de.g2p.ToSe_Parkapp.Repositories.NutzerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.cdi.Eager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class WebController {
