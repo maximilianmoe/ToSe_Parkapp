5016c20782d8b50bff36bbd54c360f601709cd1f
Matthias <accounts@matthias-scherzer.de>
2020-01-07 22:22:00 +0100

    test commit from new branch

--- src/main/java/de/g2p/ToSe_Parkapp/ToSeParkappApplication.java
+++ src/main/java/de/g2p/ToSe_Parkapp/ToSeParkappApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.mail.SimpleMailMessage;
-
+//test
 @SpringBootApplication
 @EnableJpaRepositories
 public class ToSeParkappApplication {
