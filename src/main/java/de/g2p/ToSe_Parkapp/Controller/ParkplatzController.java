a8bb4ba201bb1ffa333ecbc0277a540ca9d22e6f
Zajquor <52458920+Zajquor@users.noreply.github.com>
2020-01-11 12:30:37 +0100

    parkplaetze_medialist.html & ParkplatzController

--- src/main/java/de/g2p/ToSe_Parkapp/Controller/ParkplatzController.java
+++ src/main/java/de/g2p/ToSe_Parkapp/Controller/ParkplatzController.java
@@ -1,6 +1,9 @@
 package de.g2p.ToSe_Parkapp.Controller;
 
-import de.g2p.ToSe_Parkapp.Entities.*;
+import de.g2p.ToSe_Parkapp.Entities.Anbieter;
+import de.g2p.ToSe_Parkapp.Entities.Nutzer;
+import de.g2p.ToSe_Parkapp.Entities.Parkplatz;
+import de.g2p.ToSe_Parkapp.Entities.Standort;
 import de.g2p.ToSe_Parkapp.Repositories.AnbieterRepository;
 import de.g2p.ToSe_Parkapp.Repositories.NutzerRepository;
 import de.g2p.ToSe_Parkapp.Repositories.ParkplatzRepository;
@@ -10,8 +13,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -33,8 +40,7 @@ public class ParkplatzController {
         //Checks if anbieter already has a Parkplatz
         if (anbieter.getParkplatz() == true) {
             returnstring = "error_bereits_ein_parkplatz";
-        }
-        else {
+        } else {
             model.addAttribute("parkplatz", new Parkplatz());
             model.addAttribute("standort", new Standort());
             returnstring = "parkplatz_hinzufuegen";
@@ -57,37 +63,48 @@ public class ParkplatzController {
 //                    }
 //        }
 
-            Anbieter aid = anbieterRepository.findByNid(findNutzer());
-            parkplatz.setAnbieterId(aid);
-            parkplatz.setStatus("frei");
-            parkplatz.setOrtid(standort);
-            parkplatz.setBewertung(0);
-            parkplatz.setBewertungsanzahl(0);
+        Anbieter aid = anbieterRepository.findByNid(findNutzer());
+        parkplatz.setAnbieterId(aid);
+        parkplatz.setStatus("frei");
+        parkplatz.setOrtid(standort);
+        parkplatz.setBewertung(0);
+        parkplatz.setBewertungsanzahl(0);
 
-            //Sets Parkplatz to private if the box for "privater Parkplatz" is checked
-            if (checked.contains("1"))
-                parkplatz.setPrivat(true);
-            else if (checked.contains("2")) {
-                parkplatz.setPrivat(false);
-                parkplatz.setZeitbegrenzung(0);
-                parkplatz.setParkgebuehr(0);
-                parkplatz.setStrafgebuehr(0);
-                System.out.println(parkplatz.getStrafgebuehr());
-            }
+        //Sets Parkplatz to private if the box for "privater Parkplatz" is checked
+        if (checked.contains("1"))
+            parkplatz.setPrivat(true);
+        else if (checked.contains("2")) {
+            parkplatz.setPrivat(false);
+            parkplatz.setZeitbegrenzung(0);
+            parkplatz.setParkgebuehr(0);
+            parkplatz.setStrafgebuehr(0);
+            System.out.println(parkplatz.getStrafgebuehr());
+        }
 
-            //Sets Fahrzeugtyp if box is checked
-            if (fahrzeugtyp.contains("on"))
-                parkplatz.setFahrzeugtyp(fahrzeugtyp);
+        //Sets Fahrzeugtyp if box is checked
+        if (fahrzeugtyp.contains("on"))
+            parkplatz.setFahrzeugtyp(fahrzeugtyp);
 
-            //Saves all data in the database
-            parkplatzRepository.save(parkplatz);
-            anbieterRepository.updateParkplatz(true, aid.getAid());
-            standortRepository.save(standort);
-            return "mein_parkplatz";
+        //Saves all data in the database
+        parkplatzRepository.save(parkplatz);
+        anbieterRepository.updateParkplatz(true, aid.getAid());
+        standortRepository.save(standort);
+        return "mein_parkplatz";
     }
 
     @GetMapping("/parkplaetze_medialist")
-    public String parkMedia() {
+    public String parkMedia(Model model) {
+        List<Parkplatz> parkplaetze = parkplatzRepository.findAll();
+//        Nutzer nutzer = findNutzer();
+//        Parkplatz parkplatz = parkplatzRepository.findByAnbieterId(anbieterRepository.findByNid(nutzer.getNidNutzer()));
+//        Standort standort = standortRepository.findByOrtid(parkplatz.getOrtId());
+      /*  List<Standort> standorte = new ArrayList<Standort>();
+        for (Parkplatz parkplatz: parkplaetze) {
+            standorte = standortRepository.findByOrtid(parkplatz.getOrtId());
+        }
+        model.addAttribute("standort", standorte);
+        model.addAttribute("parkplaetze", parkplaetze);*/
+
         return "parkplaetze_medialist";
     }
 
@@ -96,10 +113,10 @@ public class ParkplatzController {
         String returnstring = "";
         Nutzer nutzer = findNutzer();
 
-        if(nutzer.getRolle().equalsIgnoreCase("anbieter"))
+        if (nutzer.getRolle().equalsIgnoreCase("anbieter"))
             returnstring = "parkplaetze_anbieter";
-        else if(nutzer.getRolle().equalsIgnoreCase("beides"))
-            returnstring="parkplaetze_beides";
+        else if (nutzer.getRolle().equalsIgnoreCase("beides"))
+            returnstring = "parkplaetze_beides";
         else if (nutzer.getRolle().equalsIgnoreCase("konsument"))
             returnstring = "parkplaetze_konsument";
 
@@ -112,7 +129,7 @@ public class ParkplatzController {
         Nutzer nutzer = findNutzer();
         Anbieter anbieter = anbieterRepository.findByNid(nutzer);
         String returnstring = "";
-        if(anbieter.getParkplatz() == false)
+        if (anbieter.getParkplatz() == false)
             returnstring = "error_noch_kein_parkplatz";
         else if (anbieter.getParkplatz() == true) {
             Parkplatz parkplatz = parkplatzRepository.findByAnbieterId(anbieterRepository.findByNid(nutzer.getNidNutzer()));
@@ -123,7 +140,7 @@ public class ParkplatzController {
         }
         return returnstring;
     }
-    
+
 
     @PostMapping("/delete_parkplatz")
     public String deleteParkplatz() {
@@ -140,7 +157,7 @@ public class ParkplatzController {
     public Nutzer findNutzer() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String benutzername = "";
-        if(principal instanceof UserDetails)
+        if (principal instanceof UserDetails)
             benutzername = ((UserDetails) principal).getUsername();
         else
             benutzername = principal.toString();
