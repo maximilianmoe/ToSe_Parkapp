bb8876a7311ed7d0dec34fb5cb132f62f2be56a6
Matthias <accounts@matthias-scherzer.de>
2020-01-28 17:28:55 +0100

    test error bilder startseite

--- src/main/java/de/g2p/ToSe_Parkapp/Configuration/SecurityConfiguration.java
+++ src/main/java/de/g2p/ToSe_Parkapp/Configuration/SecurityConfiguration.java
@@ -58,7 +58,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        String[] staticRessources = {
+        String[] staticResources = {
                 "/css/**",
                 "/images/**",
                 "/fonts/**",
